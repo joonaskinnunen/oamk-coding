006b7945a2903ffedf64d5b13f7a4f627960a87e
”Joonas <joonas.kinn@gmail.com>
2020-03-08 19:49:43 +0200

    refactoring

--- Viikko 4/vk4t32/main.c
+++ Viikko 4/vk4t32/main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-float calcSquareCirc() {
-    float length;
-    printf("Tämä ohjelma laskee neliön kehän pituuden.\nSyötä neliön sivun pituus: ");
-    scanf("%f", &length);
-    return length * 4;
-}
+float calcSquareCirc();
 
 int main()
 {
     printf("Kehän pituus on %.2f\n", calcSquareCirc());
     return 0;
+}
+
+float calcSquareCirc() {
+    float length;
+    printf("Tämä ohjelma laskee neliön kehän pituuden.\nSyötä neliön sivun pituus: ");
+    scanf("%f", &length);
+    return length * 4;
 }

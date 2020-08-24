d2d7333871b2d5803370814a3c63338b3a0aefb7
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-08-24 03:39:11 +0300

    Remove duplicate folders

--- Viikko 1/vk1t37/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    int luku = 42;
-    printf("%d\n", luku);
-    return 0;
-}
--- Viikko 6/vk6t31/uusi.h
+++
@@ -1,6 +0,0 @@
-#ifndef UUSIH //Näillä saadaan tämä tiedosto liitettyä
-#define UUSIH //vain kerran tähän ohjelmaan vain kerran
-extern int x;
-void laske();
-void tulosta();
-#endif

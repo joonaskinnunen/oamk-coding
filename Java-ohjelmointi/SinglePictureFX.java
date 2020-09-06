556c7d036511742b72bcbf9af5990780a6a65903
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-09-06 23:32:54 +0300

    Update SinglePictureFX.java program

--- Java-ohjelmointi/SinglePictureFX.java
+++ Java-ohjelmointi/SinglePictureFX.java
@@ -19,8 +19,52 @@ public class SinglePictureFX extends Application
 
       Scene scene = new Scene( group_for_imageviews, 960, 744 ) ;
 
-      scene.setFill( Color.LIGHTYELLOW ) ;
+      scene.setFill( Color.ORANGE ) ;
 
+
+      int brick_position_x  =  0 ;
+      int brick_position_y  =  0 ;
+
+      int brick_height  =   28 ;
+      int brick_length  =  112 ;
+      int gap_between_bricks =  4 ;
+      int row_counter = 0 ;
+
+      while ( brick_position_y  <  scene.getHeight() )
+      {
+         while ( brick_position_x < scene.getWidth() )
+         {
+            Rectangle new_brick =
+                        new Rectangle( brick_position_x, brick_position_y,
+                                       brick_length, brick_height ) ;
+
+            new_brick.setFill( Color.FIREBRICK ) ;
+
+            group_for_imageviews.getChildren().add( new_brick ) ;
+
+            brick_position_x = brick_position_x
+                             + brick_length + gap_between_bricks ;
+         }
+
+         // Now we must prepare to do the next row of bricks.
+         // Every second row must start with a half brick.
+         row_counter ++ ;
+
+         if ( row_counter % 2 == 1 )
+         {
+            // This row will start with a half brick.
+
+            brick_position_x = - brick_length / 2 ;
+         }
+         else
+         {
+            brick_position_x = 0 ;
+         }
+
+         brick_position_y = brick_position_y + brick_height + gap_between_bricks ;
+
+      }
+      
       Image picture_to_show = new Image( "gauguin_two_women_on_the_beach_1891.jpg" ) ;
 
       ImageView natural_size_view = new ImageView( picture_to_show ) ;

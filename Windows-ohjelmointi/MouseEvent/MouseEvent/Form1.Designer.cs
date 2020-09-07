16dad9748ec7a4debe52d9de7caa6f96757852a3
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-09-07 20:12:46 +0300

    Add MouseEvent C# program

---
+++ Windows-ohjelmointi/MouseEvent/MouseEvent/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MouseEvent
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Red;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(160, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(502, 267);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(180, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 380);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(502, 58);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Windows-ohjelmointi/MouseEvent/MouseEvent/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MouseEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.panel1.MouseEnter += new EventHandler(this.panel1_MouseEnter);
+            this.panel1.MouseLeave += new EventHandler(this.panel1_MouseLeave);
+            this.panel1.MouseMove += new MouseEventHandler(this.panel1_MouseMove);
+            this.button1.MouseEnter += new EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new EventHandler(this.button1_MouseLeave);
+            this.button1.MouseMove += new MouseEventHandler(this.button1_MouseMove);
+        }
+
+        private void panel1_MouseEnter(object sender, EventArgs e)
+        {
+            textBox1.Text = panel1.Name + ": MouseEnter";
+            Cursor = Cursors.Cross;
+        }
+
+        private void panel1_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.Text = panel1.Name + ": MouseLeave";
+            Cursor = Cursors.Default;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = panel1.Name + ": MouseMove";
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            textBox1.Text = button1.Name + ": MouseEnter";
+            Cursor = Cursors.Hand;
+
+        }
+
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            textBox1.Text = button1.Name + ": MouseLeave";
+            Cursor = Cursors.Default;
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = button1.Name + ": MouseMove";
+        }
+    }
+}

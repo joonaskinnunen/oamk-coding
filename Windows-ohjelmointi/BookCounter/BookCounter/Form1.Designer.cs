956b99743a6420b1c38d76318269963ab31ce18c
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-09-07 19:19:03 +0300

    Add BookCounter C# program

---
+++ Windows-ohjelmointi/BookCounter/BookCounter/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace BookCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.authorLabel = new System.Windows.Forms.Label();
+            this.authorTextBox = new System.Windows.Forms.TextBox();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.printButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(65, 41);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(120, 25);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Book name";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(70, 69);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(197, 31);
+            this.nameTextBox.TabIndex = 1;
+            // 
+            // authorLabel
+            // 
+            this.authorLabel.AutoSize = true;
+            this.authorLabel.Location = new System.Drawing.Point(65, 122);
+            this.authorLabel.Name = "authorLabel";
+            this.authorLabel.Size = new System.Drawing.Size(75, 25);
+            this.authorLabel.TabIndex = 2;
+            this.authorLabel.Text = "Author";
+            // 
+            // authorTextBox
+            // 
+            this.authorTextBox.Location = new System.Drawing.Point(70, 151);
+            this.authorTextBox.Name = "authorTextBox";
+            this.authorTextBox.Size = new System.Drawing.Size(197, 31);
+            this.authorTextBox.TabIndex = 3;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(289, 147);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(131, 39);
+            this.saveButton.TabIndex = 4;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(70, 213);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultsTextBox.Size = new System.Drawing.Size(472, 147);
+            this.resultsTextBox.TabIndex = 5;
+            // 
+            // printButton
+            // 
+            this.printButton.Location = new System.Drawing.Point(344, 381);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(122, 38);
+            this.printButton.TabIndex = 6;
+            this.printButton.Text = "Print";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 450);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.authorTextBox);
+            this.Controls.Add(this.authorLabel);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.nameLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.Label authorLabel;
+        private System.Windows.Forms.TextBox authorTextBox;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.Windows.Forms.Button printButton;
+    }
+}
+
---
+++ Windows-ohjelmointi/BookCounter/BookCounter/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookCounter
+{
+    public partial class Form1 : Form
+    {
+        private const int MAX_BOOK = 100;
+        private Book[] bookArray = new Book[MAX_BOOK];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            Book book = new Book(nameTextBox.Text, authorTextBox.Text);
+            if(Book.counter < MAX_BOOK - 1)
+            {
+                bookArray[book.OrderNum - 1] = book;
+                nameTextBox.Text = "";
+                authorTextBox.Text = "";
+            }
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        {
+            resultsTextBox.Text = "";
+            foreach(Book book in bookArray)
+            {
+                if(book != null)
+                {
+                    resultsTextBox.Text = resultsTextBox.Text + book.Name + " " + book.Author + " " + book.OrderNum + Environment.NewLine;
+                }
+            }
+        }
+    }
+
+    public class Book
+    {
+        private string mName;
+        private string mAuthor;
+        private int mOrderNum;
+        static public int counter = 1;
+
+        public Book(string name, string author)
+        {
+            mName = name;
+            mAuthor = author;
+            mOrderNum = counter;
+            counter++;
+        }
+        public string Name
+        {
+            get
+            {
+                return mName;
+            }
+            set
+            {
+                mName = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return mAuthor;
+            }
+            set
+            {
+                mAuthor = value;
+            }
+        }
+
+        public int OrderNum
+        {
+            get
+            {
+                return mOrderNum;
+            }
+        }
+    }
+
+}

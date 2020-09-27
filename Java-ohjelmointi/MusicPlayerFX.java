422decd84b09fbdfedc5af8190b9e806db6b17a6
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-09-27 18:48:01 +0300

    General improvements to the music player code

--- Java-ohjelmointi/MusicPlayerFX.java
+++ Java-ohjelmointi/MusicPlayerFX.java
@@ -11,14 +11,12 @@ import java.util.Random;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
-import javafx.geometry.HPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
@@ -27,10 +25,9 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
 import javafx.util.Duration;
 
 /**
@@ -45,15 +42,16 @@ public class MusicPlayerFX extends Application {
     ArrayList<Integer> trackHistory = new ArrayList<>();
     Media sound = new Media(new File("sounds/" + tracks[0]).toURI().toString());
     MediaPlayer mediaPlayer = new MediaPlayer(sound);
-    Label trackName = new Label("Paused " + tracks[0]);
+    Label trackNameLabel = new Label("Paused " + tracks[0]);
     Slider timeSlider = new Slider(0, 0, 0);
     Button playButton = new Button("play");
     Button nextButton = new Button("next");
     Button previousButton = new Button("prev");
     Button volumeDownButton = new Button();
     Button volumeUpButton = new Button();
-    Label title = new Label("Music player");
+    Label titleLabel = new Label("Music player");
     Label volumeLabel = new Label("Volume: " + (int) Math.round(mediaPlayer.getVolume() * 100) + "%");
+    Label timeLabel = new Label();
     Image vUpImg = new Image("https://i.ibb.co/X2W6L2Z/volumeup.png");
     ImageView vUpView = new ImageView(vUpImg);
     Image vDownImg = new Image("https://i.ibb.co/yPNX3JW/volumedown.png");
@@ -70,7 +68,7 @@ public class MusicPlayerFX extends Application {
     CheckBox shuffleCb = new CheckBox("shuffle");
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
+    public void start(Stage stage) throws Exception {
         vUpView.setFitHeight(10);
         vUpView.setPreserveRatio(true);
         vDownView.setFitHeight(10);
@@ -88,16 +86,17 @@ public class MusicPlayerFX extends Application {
         playButton.setGraphic(playView);
         nextButton.setGraphic(nextView);
         previousButton.setGraphic(prevView);
-        timeSlider.setMinWidth(50);
-        timeSlider.setMaxWidth(Double.MAX_VALUE);
-        createMediaPlayer();
+        timeSlider.setMinWidth(200);
+        timeSlider.setMaxWidth(250);
         
+        createMediaPlayer();
+
         previousButton.setDisable(true);
 
         playButton.setOnAction((ActionEvent event)
                 -> {
             isPlaying = !isPlaying;
-            trackName.setText(isPlaying ? "Now playing: " + tracks[trackIndex] : "Paused: " + tracks[trackIndex]);
+            trackNameLabel.setText(isPlaying ? "Now playing: " + tracks[trackIndex] : "Paused: " + tracks[trackIndex]);
             playButton.setText(isPlaying ? "pause" : "play");
             playButton.setGraphic(isPlaying ? pauseView : playView);
             togglePlayer();
@@ -148,28 +147,33 @@ public class MusicPlayerFX extends Application {
             System.out.println("previous track");
             trackIndex = trackHistory.get(trackHistory.size() - 1);
             trackHistory.remove(trackHistory.size() - 1);
-            if(trackHistory.size() == 0) previousButton.setDisable(true);
+            if (trackHistory.isEmpty()) {
+                previousButton.setDisable(true);
+            }
             createMediaPlayer();
             togglePlayer();
         });
 
-        GridPane grid = new GridPane();
-        grid.setAlignment(Pos.TOP_CENTER);
-        grid.setStyle("-fx-border-style: solid inside;"
+        VBox vbox = new VBox();
+        HBox hbButtons = new HBox();
+        HBox hbVolumeButtons = new HBox();
+        HBox hbCheckBoxes = new HBox();
+
+        vbox.setStyle("-fx-border-style: solid inside;"
                 + "-fx-border-width: 2;"
                 + "-fx-border-insets: 5;"
                 + "-fx-border-radius: 5;"
                 + "-fx-border-color: orange;"
-                + "-fx-background-color: black;");
-        GridPane.setHalignment(title, HPos.CENTER);
-        GridPane.setHalignment(trackName, HPos.CENTER);
-        GridPane.setHalignment(volumeLabel, HPos.CENTER);
-        title.setFont(Font.font("Tahoma", 30));
-        trackName.setFont(Font.font("Tahoma", 15));
-        title.setMinHeight(70);
-        trackName.setTextFill(Color.ORANGE);
-        title.setTextFill(Color.WHITE);
-        trackName.setBackground(new Background(
+                + "-fx-background-color: black;"
+                + "-fx-alignment: center");
+        titleLabel.setStyle("-fx-padding: 0 0 15 0;"
+                + "-fx-font: 30px Tahoma;"
+                + "-fx-text-fill: white");
+        trackNameLabel.setStyle("-fx-font: 15px Tahoma;"
+                + "-fx-text-fill: orange");
+        timeLabel.setStyle("-fx-font: 10px Tahoma;"
+                + "-fx-text-fill: white");
+        trackNameLabel.setBackground(new Background(
                 new BackgroundFill(
                         Color.color(
                                 Color.YELLOW.getRed(),
@@ -177,78 +181,88 @@ public class MusicPlayerFX extends Application {
                                 Color.YELLOW.getBlue(), 0.4d),
                         new CornerRadii(2),
                         null)));
+        hbButtons.setStyle("-fx-padding: 10 0 10 0;"
+                + "-fx-spacing: 10;"
+                + "-fx-alignment: center");
+        hbVolumeButtons.setStyle("-fx-padding: 5 0 0 0;"
+                + "-fx-spacing: 10;"
+                + "-fx-alignment: center");
+        hbCheckBoxes.setStyle("-fx-padding: 5 0 15 0;"
+                + "-fx-spacing: 10;"
+                + "-fx-alignment: center");
+
         volumeLabel.setTextFill(Color.WHITE);
-        HBox hbButtons = new HBox();
-        HBox hbVolumeButtons = new HBox();
-        HBox hbCheckBoxes = new HBox();
-        hbButtons.setSpacing(10.0);
-        hbButtons.setPadding(new Insets(20, 0, 20, 0));
-        hbButtons.getChildren().addAll(playButton, previousButton, nextButton);
-        hbVolumeButtons.setSpacing(10.0);
-        hbVolumeButtons.setPadding(new Insets(5, 0, 0, 0));
-        hbVolumeButtons.setAlignment(Pos.CENTER);
-        hbVolumeButtons.getChildren().addAll(volumeDownButton, volumeUpButton);
-        hbCheckBoxes.setSpacing(10.0);
-        hbCheckBoxes.setPadding(new Insets(5, 0, 0, 0));
-        hbCheckBoxes.setAlignment(Pos.CENTER);
-        hbCheckBoxes.getChildren().addAll(autoplayCb, shuffleCb);
         autoplayCb.setTextFill(Color.WHITE);
         shuffleCb.setTextFill(Color.WHITE);
-        timeSlider.setPadding(new Insets(5, 0, 0, 0));
-        hbCheckBoxes.setPadding(new Insets(0, 0, 15, 0));
-        grid.add(title, 1, 0);
-        grid.add(trackName, 1, 1);
-        grid.add(timeSlider, 1, 2);
-        grid.add(hbButtons, 1, 3);
-        grid.add(hbCheckBoxes, 1, 4);
-        grid.add(volumeLabel, 1, 5);
-        grid.add(hbVolumeButtons, 1, 6);
-        Scene scene = new Scene(grid, 300, 300);
-        primaryStage.setTitle("Music player");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        timeSlider.setPadding(new Insets(25, 0, 0, 0));
+
+        hbButtons.getChildren().addAll(playButton, previousButton, nextButton);
+        hbCheckBoxes.getChildren().addAll(autoplayCb, shuffleCb);
+        hbVolumeButtons.getChildren().addAll(volumeDownButton, volumeUpButton);
+
+        vbox.getChildren().addAll(titleLabel, trackNameLabel, timeSlider, timeLabel, hbButtons, hbCheckBoxes, volumeLabel, hbVolumeButtons);
+
+        Scene scene = new Scene(vbox, 300, 300);
+        stage.setTitle("Music player");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public void createMediaPlayer() {
         double volume = mediaPlayer.getVolume();
         mediaPlayer.dispose();
         sound = new Media(new File("sounds/" + tracks[trackIndex]).toURI().toString());
-        trackName.setText(isPlaying ? "Now playing: " + tracks[trackIndex] : "Paused: " + tracks[trackIndex]);
+        trackNameLabel.setText(isPlaying ? "Now playing: " + tracks[trackIndex] : "Paused: " + tracks[trackIndex]);
         mediaPlayer = new MediaPlayer(sound);
         mediaPlayer.setVolume(volume);
+
         sound.durationProperty().addListener(change -> {
             timeSlider.setMax(sound.getDuration().toSeconds());
             timeSlider.setOnMouseDragged(drag -> {
                 mediaPlayer.seek(Duration.seconds(timeSlider.getValue()));
             });
         });
+
         mediaPlayer.currentTimeProperty().addListener(c -> {
+            timeLabel.setText(getTimeString((long) mediaPlayer.getCurrentTime().toMillis()) + "/" + getTimeString((long) sound.getDuration().toMillis()));
             timeSlider.setValue(mediaPlayer.getCurrentTime().toSeconds());
-            if (mediaPlayer.getCurrentTime().toSeconds() == sound.getDuration().toSeconds()) {
-                if(autoplayCb.isSelected()) {
-                    nextButton.fire();
-                } else {
-                    mediaPlayer.seek(Duration.seconds(0));
-                        playButton.setText("play");
-                        playButton.setGraphic(playView);
-                }
+
+        });
+
+        mediaPlayer.setOnEndOfMedia(() -> {
+            if (autoplayCb.isSelected()) {
+                nextButton.fire();
+            } else {
+                mediaPlayer.seek(Duration.seconds(0));
+                timeSlider.setValue(0);
+                playButton.fire();
+                timeLabel.setText(getTimeString((long) mediaPlayer.getCurrentTime().toMillis()) + "/" + getTimeString((long) sound.getDuration().toMillis()));
             }
         });
     }
 
-    public void togglePlayer() {
-        try {
-            if (isPlaying) {
-                mediaPlayer.play();
-                System.out.println("playing..");
-            } else {
-                mediaPlayer.pause();
-                System.out.println("paused");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public String getTimeString(long millis) {
+        StringBuilder str = new StringBuilder();
 
+        int minutes = (int) ((millis % (1000 * 60 * 60)) / (1000 * 60));
+        int seconds = (int) (((millis % (1000 * 60 * 60)) % (1000 * 60)) / 1000);
+
+        str
+                .append(String.format("%02d", minutes))
+                .append(":")
+                .append(String.format("%02d", seconds));
+
+        return str.toString();
+    }
+
+    public void togglePlayer() {
+        if (isPlaying) {
+            mediaPlayer.play();
+            System.out.println("playing");
+        } else {
+            mediaPlayer.pause();
+            System.out.println("paused");
+        }
     }
 
     public static void main(String[] args) {

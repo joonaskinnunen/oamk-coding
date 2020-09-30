3b70b1103e333d3f20ffe3cec78c1341b3aa2445
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-09-30 21:36:46 +0300

    Add CalculatorStatic C# program

---
+++ Windows-ohjelmointi/CalculatorStatic/CalculatorStatic/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CalculatorStatic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonMinus = new System.Windows.Forms.Button();
+            this.buttonMul = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 151);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(124, 151);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(18, 218);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(47, 39);
+            this.buttonAdd.TabIndex = 2;
+            this.buttonAdd.Text = "+";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonMinus
+            // 
+            this.buttonMinus.Location = new System.Drawing.Point(71, 218);
+            this.buttonMinus.Name = "buttonMinus";
+            this.buttonMinus.Size = new System.Drawing.Size(47, 39);
+            this.buttonMinus.TabIndex = 3;
+            this.buttonMinus.Text = "-";
+            this.buttonMinus.UseVisualStyleBackColor = true;
+            this.buttonMinus.Click += new System.EventHandler(this.buttonMinus_Click);
+            // 
+            // buttonMul
+            // 
+            this.buttonMul.Location = new System.Drawing.Point(124, 218);
+            this.buttonMul.Name = "buttonMul";
+            this.buttonMul.Size = new System.Drawing.Size(47, 39);
+            this.buttonMul.TabIndex = 4;
+            this.buttonMul.Text = "*";
+            this.buttonMul.UseVisualStyleBackColor = true;
+            this.buttonMul.Click += new System.EventHandler(this.buttonMul_Click);
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.Location = new System.Drawing.Point(177, 218);
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.Size = new System.Drawing.Size(47, 39);
+            this.buttonDiv.TabIndex = 5;
+            this.buttonDiv.Text = "/";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.buttonDiv_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(18, 298);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(0, 25);
+            this.labelResult.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(750, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Enter a decimal numbers and select an aritmetic operation from into the fields";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelResult);
+            this.Controls.Add(this.buttonDiv);
+            this.Controls.Add(this.buttonMul);
+            this.Controls.Add(this.buttonMinus);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button buttonMul;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Windows-ohjelmointi/CalculatorStatic/CalculatorStatic/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorStatic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            double firstInput, secondInput;
+            if (!Double.TryParse(textBox1.Text, out firstInput))
+            {
+                labelResult.Text = "First input invalid. Hint: use comma instead of dot.";
+            }
+            else if (!Double.TryParse(textBox2.Text, out secondInput))
+            {
+                labelResult.Text = "Second input invalid. Hint: use comma instead of dot.";
+            }
+            else
+            {
+                labelResult.Text = "Result is: " + Calculate.Add(firstInput, secondInput);
+            }
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            double firstInput, secondInput;
+            if (!Double.TryParse(textBox1.Text, out firstInput))
+            {
+                labelResult.Text = "First input invalid. Hint: use comma instead of dot.";
+            }
+            else if (!Double.TryParse(textBox2.Text, out secondInput))
+            {
+                labelResult.Text = "Second input invalid. Hint: use comma instead of dot.";
+            }
+            else
+            {
+                labelResult.Text = "Result is: " + Calculate.Minus(firstInput, secondInput);
+            }
+        }
+
+        private void buttonMul_Click(object sender, EventArgs e)
+        {
+            double firstInput, secondInput;
+            if (!Double.TryParse(textBox1.Text, out firstInput))
+            {
+                labelResult.Text = "First input invalid. Hint: use comma instead of dot.";
+            }
+            else if (!Double.TryParse(textBox2.Text, out secondInput))
+            {
+                labelResult.Text = "Second input invalid. Hint: use comma instead of dot.";
+            }
+            else
+            {
+                labelResult.Text = "Result is: " + Calculate.Mul(firstInput, secondInput);
+            }
+        }
+
+        private void buttonDiv_Click(object sender, EventArgs e)
+        {
+            double firstInput, secondInput;
+            if (!Double.TryParse(textBox1.Text, out firstInput))
+            {
+                labelResult.Text = "First input invalid. Hint: use comma instead of dot.";
+            }
+            else if (!Double.TryParse(textBox2.Text, out secondInput))
+            {
+                labelResult.Text = "Second input invalid. Hint: use comma instead of dot.";
+            }
+            else
+            {
+                labelResult.Text = "Result is: " + Calculate.Div(firstInput, secondInput);
+            }
+        }
+    }
+
+    public class Calculate
+    {
+        public static double Add(double firstNum, double secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        public static double Minus(double firstNum, double secondNum)
+        {
+            return firstNum - secondNum;
+        }
+
+        public static double Mul(double firstNum, double secondNum)
+        {
+            return firstNum * secondNum;
+        }
+
+        public static double Div(double firstNum, double secondNum)
+        {
+            return firstNum / secondNum;
+        }
+    }
+}

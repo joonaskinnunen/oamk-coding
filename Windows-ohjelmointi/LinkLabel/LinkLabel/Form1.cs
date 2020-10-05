400846d43402495adf60992d0f3f9ee3ab4903fe
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-10-05 23:45:06 +0300

    Add LinkLabel C# program

---
+++ Windows-ohjelmointi/LinkLabel/LinkLabel/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LinkLabel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Text = "Ilta-sanomat";
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Text = "Excel";
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://is.fi");
+
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"C:\Users\Joonas Kinnunen\Downloads\excelfile.xlsx");
+        }
+    }
+}

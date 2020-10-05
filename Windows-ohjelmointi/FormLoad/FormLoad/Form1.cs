ae1ed4bf5c41afbe206ce10b61741d6b4116014b
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-10-05 21:52:45 +0300

    Add FormLoad C# program

---
+++ Windows-ohjelmointi/FormLoad/FormLoad/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormLoad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.AppendText("Tom Jones");
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("John Smith");
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText("David Osborne");
+        }
+    }
+}

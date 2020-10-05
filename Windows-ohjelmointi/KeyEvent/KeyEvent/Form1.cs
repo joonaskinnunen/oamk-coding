6445658ac28d0a443bf29eba040101dfb4d764ed
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-10-05 22:25:28 +0300

    Add KeyEvent C# program

---
+++ Windows-ohjelmointi/KeyEvent/KeyEvent/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.KeyUp += textBox1_KeyUp;
+        }
+
+        private void textBox1_KeyUp(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            textBox2.Text = textBox1.Text;
+        }
+    }
+}
